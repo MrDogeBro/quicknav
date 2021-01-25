fad32ff7eced0eee19135a57dcaaba9bfb514c48
MrDogeBro <MrDogeBro@users.noreply.github.com>
2021-01-25 14:26:26 -0500

    Add optional param to find specific shortcut

--- src/Commands/list.rs
+++ src/Commands/list.rs
@@ -1,5 +1,6 @@
 use std::fs::File;
 use std::env::var;
+use std::process::exit;
 use prettytable::{Table};
 use prettytable::format;
 
@@ -16,21 +17,45 @@ struct Config {
     shortcuts: Vec<Shortcut>
 }
 
-pub fn list() {
-    let config_folder = var("XDG_CONFIG_HOME").or_else(|_| var("HOME").map(|home|format!("{}/.config", home))).unwrap();
-    let config_path = format!("{}/quicknav/quicknav.json", config_folder);
+pub fn list(shortcut: Option<String>) {
+    if let Some(call) = shortcut {
+        let config_folder = var("XDG_CONFIG_HOME").or_else(|_| var("HOME").map(|home|format!("{}/.config", home))).unwrap();
+        let config_path = format!("{}/quicknav/quicknav.json", config_folder);
 
-    let data = File::open(config_path).expect("Error: Unable to open config file.");
-    let config: Config = serde_json::from_reader(data).expect("Error: Unable to read config file.");
+        let data = File::open(config_path).expect("Error: Unable to open config file.");
+        let config: Config = serde_json::from_reader(data).expect("Error: Unable to read config file.");
 
-    let mut shortcut_list = Table::new();
-    shortcut_list.set_format(*format::consts::FORMAT_NO_BORDER_LINE_SEPARATOR);
-    shortcut_list.set_titles(row!["Shortcuts", "Shortcut Name", "Shortcut Location"]);
+        let mut shortcut_list = Table::new();
+        shortcut_list.set_format(*format::consts::FORMAT_NO_BORDER_LINE_SEPARATOR);
+        shortcut_list.set_titles(row!["Shortcuts", "Shortcut Name", "Shortcut Location"]);
 
-    for shortcut in config.shortcuts {
-        let calls: String = shortcut.calls.join(", ");
-        shortcut_list.add_row(row![calls, shortcut.name, shortcut.location]);
+        for shortcut_conf in config.shortcuts {
+            if shortcut_conf.calls.iter().any(|c| c == &call) {
+                let calls: String = shortcut_conf.calls.join(", ");
+                shortcut_list.add_row(row![calls, shortcut_conf.name, shortcut_conf.location]);
+                shortcut_list.printstd();
+                exit(0)
+            }
+        }
+
+        exit(1)
+    } else {
+        let config_folder = var("XDG_CONFIG_HOME").or_else(|_| var("HOME").map(|home|format!("{}/.config", home))).unwrap();
+        let config_path = format!("{}/quicknav/quicknav.json", config_folder);
+
+        let data = File::open(config_path).expect("Error: Unable to open config file.");
+        let config: Config = serde_json::from_reader(data).expect("Error: Unable to read config file.");
+
+        let mut shortcut_list = Table::new();
+        shortcut_list.set_format(*format::consts::FORMAT_NO_BORDER_LINE_SEPARATOR);
+        shortcut_list.set_titles(row!["Shortcuts", "Shortcut Name", "Shortcut Location"]);
+
+        for shortcut_conf in config.shortcuts {
+            let calls: String = shortcut_conf.calls.join(", ");
+            shortcut_list.add_row(row![calls, shortcut_conf.name, shortcut_conf.location]);
+        }
+
+        shortcut_list.printstd();
+        exit(0)
     }
-
-    shortcut_list.printstd();
 }
