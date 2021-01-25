f91a824e68bda410418c66db972db21eb6f0ace7
MrDogeBro <MrDogeBro@users.noreply.github.com>
2021-01-24 19:58:22 -0500

    Load shell profile from .txt file based on command input

--- src/Commands/init.rs
+++ src/Commands/init.rs
@@ -1,26 +1,9 @@
-pub fn init() {
-    let shell = r#"function __quicknav_cd() {
-  cd "$@"
-}
+pub fn init(shell: String) {
+    let shell_default = include_str!("../../shell/default.txt");
 
-function __quicknav_nav() {
-  if [ "$#" -eq 0 ]; then
-    quicknav --help
-  else
-    local __quicknav_result
-    __quicknav_result="$(quicknav get "$@")"
-
-    if [[ $__quicknav_result == /* ]]; then
-      __quicknav_cd "$__quicknav_result"
-    else
-      echo "\033[0;31m$__quicknav_result"
-    fi
-  fi
-}
-
-function nav() {
-  __quicknav_nav "$@"
-}"#;
-
-    println!("{}", shell)
+    if shell == "bash" || shell == "zsh" {
+        println!("{}", shell_default)
+    } else {
+        println!("echo -e \"\\033[0;31mError: Failed to load shell profile. Invalid profile provided.\"")
+    }
 }
