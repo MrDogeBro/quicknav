af386ba1ad8eb7edc651dd6c1d86b773d9744627
MrDogeBro <MrDogeBro@users.noreply.github.com>
2021-01-25 11:00:26 -0500

    Include pretty table crate

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,7 @@
 extern crate serde;
 extern crate serde_json;
-#[macro_use]
-extern crate serde_derive;
+#[macro_use] extern crate serde_derive;
+#[macro_use] extern crate prettytable;
 
 mod commands;
 
