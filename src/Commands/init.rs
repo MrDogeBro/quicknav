cc9bd0af4b34af7bf0dd12c89c002112c527bca0
MrDogeBro <MrDogeBro@users.noreply.github.com>
2021-01-28 18:30:48 -0500

    Add option to change the nav invoke command

--- src/Commands/init.rs
+++ src/Commands/init.rs
@@ -1,9 +1,32 @@
-pub fn init(shell: String) {
-    let shell_default = include_str!("../../shell/default.txt");
+fn get_profile(profile: &str, command: &str) -> String {
+    if profile == "default" {
+        let shell_profile = include_str!("../../shell/default.txt");
+
+        if command.len() > 0 {
+            let new_command = format!("function {}", command);
+            return shell_profile.replace("function nav", &new_command);
+        }
+
+        return shell_profile.to_string();
+    }
+
+    return "".to_string();
+}
+
+pub fn init(shell: String, command: Option<String>) {
+    let mut profile: &str = "default";
 
     if shell == "bash" || shell == "zsh" {
-        println!("{}", shell_default)
+        profile = "default"
     } else {
-        println!("echo -e \"\\033[0;31mError: Failed to load shell profile. Invalid profile provided.\"")
+        println!(
+            "echo -e \"\\033[0;31mError: Failed to load shell profile. Invalid profile provided.\""
+        )
+    }
+
+    if let Some(cmd) = command {
+        println!("{}", get_profile(profile, &cmd));
+    } else {
+        println!("{}", get_profile(profile, ""));
     }
 }
