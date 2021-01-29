96a55f6b650b6052744c33c6aacaf7891be8d4b5
MrDogeBro <MrDogeBro@users.noreply.github.com>
2021-01-29 15:35:21 -0500

    Add support for fish shell

--- src/Commands/init.rs
+++ src/Commands/init.rs
@@ -8,6 +8,15 @@ fn get_profile(profile: &str, command: &str) -> String {
         }
 
         return shell_profile.to_string();
+    } else if profile == "fish" {
+        let shell_profile = include_str!("../../shell/fish.txt");
+
+        if command.len() > 0 {
+            let new_command = format!("function {}", command);
+            return shell_profile.replace("function nav", &new_command);
+        }
+
+        return shell_profile.to_string();
     }
 
     return "".to_string();
@@ -18,10 +27,8 @@ pub fn init(shell: String, command: Option<String>) {
 
     if shell == "bash" || shell == "zsh" {
         profile = "default"
-    } else {
-        println!(
-            "echo -e \"\\033[0;31mError: Failed to load shell profile. Invalid profile provided.\""
-        )
+    } else if shell == "fish" {
+        profile = "fish"
     }
 
     if let Some(cmd) = command {
