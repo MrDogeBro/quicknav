d9bbfd87e4d38cdba113eb4ec061048682017b4f
MrDogeBro <MrDogeBro@users.noreply.github.com>
2021-02-28 10:14:16 -0500

    Use config file for everything config related

--- src/commands/get.rs
+++ src/commands/get.rs
@@ -1,50 +1,12 @@
 use colored::*;
 use std::env::var;
 use std::fs;
-use std::fs::File;
-use std::path::Path;
 use std::process::exit;
 
-#[derive(Serialize, Deserialize)]
-struct Shortcut {
-    name: String,
-    description: String,
-    location: String,
-    calls: Vec<String>,
-}
-
-#[derive(Serialize, Deserialize)]
-struct Config {
-    shortcuts: Vec<Shortcut>,
-}
-
-fn generate_config() {
-    let config_folder = var("XDG_CONFIG_HOME")
-        .or_else(|_| var("HOME").map(|home| format!("{}/.config", home)))
-        .unwrap();
-    let config_path = format!("{}/quicknav", config_folder);
-    fs::create_dir(&config_path).expect("Error: Unable to generate config directory.");
-    fs::write(
-        format!("{}/quicknav.json", &config_path),
-        r#"{
-    "shortcuts": []
-}"#,
-    )
-    .expect("Error: Unable to generate config.");
-}
+use crate::config;
 
 pub fn get(location: String) {
-    let config_folder = var("XDG_CONFIG_HOME")
-        .or_else(|_| var("HOME").map(|home| format!("{}/.config", home)))
-        .unwrap();
-    let config_path = format!("{}/quicknav/quicknav.json", config_folder);
-
-    if !Path::new(&config_path).exists() {
-        generate_config();
-    }
-
-    let data = File::open(config_path).expect("Error: Unable to open config file.");
-    let config: Config = serde_json::from_reader(data).expect("Error: Unable to read config file.");
+    let config: config::Config = config::load_config();
 
     for shortcut in config.shortcuts {
         if shortcut.calls.iter().any(|c| c == &location) {
