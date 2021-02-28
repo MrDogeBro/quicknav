57278b9f7bc09e7fed813efafa2f70456bb6e17e
MrDogeBro <MrDogeBro@users.noreply.github.com>
2021-02-28 10:34:37 -0500

    Add logic to create missing dirs if enabled in config file

--- src/commands/get.rs
+++ src/commands/get.rs
@@ -1,5 +1,7 @@
 use colored::*;
 use std::env::var;
+use std::fs::create_dir_all;
+use std::path::Path;
 use std::process::exit;
 
 use crate::config;
@@ -9,8 +11,35 @@ pub fn get(location: String) {
 
     for shortcut in config.shortcuts {
         if shortcut.calls.iter().any(|c| c == &location) {
+            let shortcut_location = shortcut.location.replace("~", &var("HOME").unwrap());
+
+            if Path::new(&shortcut_location).exists() {
+                println!("{}", shortcut.location.replace("~", &var("HOME").unwrap()));
+                exit(0);
+            }
+
+            if !config.options.create_missing_directories {
+                println!(
+                    "{} {}{}",
+                    "Error: Shortcut location does not exist".red(),
+                    &shortcut_location.red(),
+                    ". If you would like quicknav to automatically create missing directories for you, enable the option create_missing_directories in your config file.".red()
+                );
+                exit(1);
+            }
+
+            if let Err(e) = create_dir_all(&shortcut_location) {
+                println!(
+                    "{} {}. Traceback: {}",
+                    "Error: Could not create directories for path".red(),
+                    &shortcut_location.red(),
+                    e.to_string().red()
+                );
+                exit(1);
+            }
+
             println!("{}", shortcut.location.replace("~", &var("HOME").unwrap()));
-            exit(0)
+            exit(0);
         }
     }
 
