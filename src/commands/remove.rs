85dc1c2b938a91fa2b8312532f60ac47fa29fc59
MrDogeBro <MrDogeBro@users.noreply.github.com>
2021-02-28 10:47:13 -0500

    Update to use config handler for removing shortcuts

--- src/commands/remove.rs
+++ src/commands/remove.rs
@@ -1,51 +1,10 @@
 use colored::*;
-use std::env;
-use std::fs;
-use std::fs::File;
-use std::path::Path;
 use std::process::exit;
 
-#[derive(Serialize, Deserialize)]
-struct Shortcut {
-    name: String,
-    description: String,
-    location: String,
-    calls: Vec<String>,
-}
-
-#[derive(Serialize, Deserialize)]
-struct Config {
-    shortcuts: Vec<Shortcut>,
-}
-
-fn generate_config() {
-    let config_folder = env::var("XDG_CONFIG_HOME")
-        .or_else(|_| env::var("HOME").map(|home| format!("{}/.config", home)))
-        .unwrap();
-    let config_path = format!("{}/quicknav", config_folder);
-    fs::create_dir(&config_path).expect("Error: Unable to generate config directory.");
-    fs::write(
-        format!("{}/quicknav.json", &config_path),
-        r#"{
-    "shortcuts": []
-}"#,
-    )
-    .expect("Error: Unable to generate config.");
-}
+use crate::config;
 
 pub fn remove(shortcut: String) {
-    let config_folder = env::var("XDG_CONFIG_HOME")
-        .or_else(|_| env::var("HOME").map(|home| format!("{}/.config", home)))
-        .unwrap();
-    let config_path = format!("{}/quicknav/quicknav.json", config_folder);
-
-    if !Path::new(&config_path).exists() {
-        generate_config();
-    }
-
-    let data = File::open(&config_path).expect("Error: Unable to open config file.");
-    let mut config: Config =
-        serde_json::from_reader(data).expect("Error: Unable to read config file.");
+    let mut config: config::Config = config::load_config();
     let mut found_shortcut = false;
     let mut index_to_remove: usize = 0;
 
@@ -67,8 +26,7 @@ pub fn remove(shortcut: String) {
     }
 
     config.shortcuts.remove(index_to_remove);
-    fs::write(config_path, serde_json::to_string_pretty(&config).unwrap())
-        .expect("Error: Failed to write config to file.");
+    config::update_config(config);
     println!("{} {}", "Shortcut removed:".green(), &shortcut);
     exit(0)
 }
