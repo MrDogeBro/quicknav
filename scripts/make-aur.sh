3d94c0e03b14382ad9285337eb0a02324d0d8f1d
MrDogeBro <MrDogeBro@users.noreply.github.com>
2021-03-06 14:59:39 -0500

    Add shell script that makes files for aur

---
+++ scripts/make-aur.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+if [ -x "$(command -v cargo-aur)" ]; then
+  command cargo aur
+
+  if [ $? -eq 0 ]; then
+    command cd ../aur-quicknav
+
+    if [ -f PKGBUILD ]; then
+      command rm PKGBUILD
+    fi
+
+    if [ -f .SRCINFO ]; then
+      command rm .SRCINFO
+    fi
+
+    command mv ../main-quicknav/PKGBUILD PKGBUILD
+    command makepkg --printsrcinfo > .SRCINFO
+  fi
+fi
