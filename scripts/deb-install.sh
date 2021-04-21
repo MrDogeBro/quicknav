301d2443376515d3d8b79e85398105c9a62d2ff4
MrDogeBro <MrDogeBro@users.noreply.github.com>
2021-04-21 19:42:28 -0400

    Add logic to ask user if they want to install

--- scripts/deb-install.sh
+++ scripts/deb-install.sh
@@ -7,9 +7,10 @@ DEF="\033[0m"
 BOLD="\033[1m"
 
 # if [ -f "/etc/debian_version" ]; then
-  tag=$(curl -s https://api.github.com/repos/MrDogeBro/quicknav/releases/latest |
-    awk '/tag_name/ { tag = $2 } /tag_name/ {print tag}' |
-    cut -d '"' -f2)
+  # tag=$(curl -s https://api.github.com/repos/MrDogeBro/quicknav/releases/latest |
+    # awk '/tag_name/ { tag = $2 } /tag_name/ {print tag}' |
+    # cut -d '"' -f2)
+  tag="v0.1.0"
   # platformList=($(curl -s https://api.github.com/repos/MrDogeBro/quicknav/releases/latest |
     # awk '/name/ { name = $2 } /name/ { print name }' |
     # awk '/.deb/' |
@@ -20,6 +21,7 @@ BOLD="\033[1m"
     cut -d '"' -f2))
   platformStr=""
   platformIndex=1
+  platformChoice=0
 
   for platform in "${platformList[@]}"; do
       platformStr+="[${platformIndex}] $platform "
@@ -33,17 +35,30 @@ BOLD="\033[1m"
   echo -e -n "➤ ${GREEN}${BOLD}Choose deb file platform (default=1)${DEF}: "
   read -n 1 -p "" debPlatform
 
-  if [ "$debPlatform" == "1" ] || [ "$debPlatform" == "" ]; then
-    if [ "$debPlatform" != "" ]; then
-      echo -e -n "\n"
-    fi
+  if [ "$debPlatform" != "" ]; then
+    echo -e -n "\n"
 
     for (( i=0; i<${#platformList[@]}; i++ )); do
-      echo $i
+      if [ $(( ${debPlatform} )) -eq $(( ${i} + 1 )) ]; then
+        platformChoice=$i
+      fi
     done
   fi
 
-  echo -e "➤ ${BLUE}${BOLD}Info${DEF}: Downloading deb file..."
+  echo -e -n "➤ ${GREEN}${BOLD}Would you like to install ${platformList[$platformChoice]} (Y/n)${DEF}: "
+  read -n 1 -p "" installDeb
+
+  if [ "$installDeb" != "" ]; then
+    echo -e -n "\n"
+
+    if [ "$installDeb" != "y" ]; then
+      exit
+    fi
+  fi
+
+  echo -e "\n➤ ┌ Installing ${platformList[$platformChoice]}"
+
+  echo -e "➤ │ ${BLUE}${BOLD}Info${DEF}: Downloading deb file..."
 # else
   # echo -e "${RED}It appears you are not on a debian based system. This script only works on debian based systems. Please check out the section on installing quicknav to get instructions for your OS. Thanks! \n\n${GREEN}Installing Quicknav${DEF}: https://github.com/MrDogeBro/quicknav#installing-quicknav"
 # fi
