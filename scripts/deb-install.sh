3e283d05890178ad4f2404fe6ab4071075e6f759
MrDogeBro <MrDogeBro@users.noreply.github.com>
2021-04-21 20:25:41 -0400

    Add final changes to script

--- scripts/deb-install.sh
+++ scripts/deb-install.sh
@@ -3,19 +3,15 @@
 RED="\033[0;31m"
 GREEN="\033[0;32m"
 BLUE="\033[0;34m"
+DIM="\033[2m"
 DEF="\033[0m"
 BOLD="\033[1m"
 
-# if [ -f "/etc/debian_version" ]; then
-  # tag=$(curl -s https://api.github.com/repos/MrDogeBro/quicknav/releases/latest |
-    # awk '/tag_name/ { tag = $2 } /tag_name/ {print tag}' |
-    # cut -d '"' -f2)
-  tag="v0.1.0"
-  # platformList=($(curl -s https://api.github.com/repos/MrDogeBro/quicknav/releases/latest |
-    # awk '/name/ { name = $2 } /name/ { print name }' |
-    # awk '/.deb/' |
-    # cut -d '"' -f2))
-  platformList=($(cat git.txt |
+if [ -f "/etc/debian_version" ]; then
+  tag=$(curl -s https://api.github.com/repos/MrDogeBro/quicknav/releases/latest |
+    awk '/tag_name/ { tag = $2 } /tag_name/ {print tag}' |
+    cut -d '"' -f2)
+  platformList=($(curl -s https://api.github.com/repos/MrDogeBro/quicknav/releases/latest |
     awk '/name/ { name = $2 } /name/ { print name }' |
     awk '/.deb/' |
     cut -d '"' -f2))
@@ -52,6 +48,7 @@ BOLD="\033[1m"
     echo -e -n "\n"
 
     if [ "$installDeb" != "y" ]; then
+      echo -e "➤ ${RED}Stopping install...${DEF}"
       exit
     fi
   fi
@@ -59,6 +56,28 @@ BOLD="\033[1m"
   echo -e "\n➤ ┌ Installing ${platformList[$platformChoice]}"
 
   echo -e "➤ │ ${BLUE}${BOLD}Info${DEF}: Downloading deb file..."
-# else
-  # echo -e "${RED}It appears you are not on a debian based system. This script only works on debian based systems. Please check out the section on installing quicknav to get instructions for your OS. Thanks! \n\n${GREEN}Installing Quicknav${DEF}: https://github.com/MrDogeBro/quicknav#installing-quicknav"
-# fi
+  echo -e "➤ │ ${DIM}$ curl -L -o /tmp/quicknav-installer.deb \"https://github.com/MrDogeBro/quicknav/releases/download/${tag}/${platformList[$platformChoice]}\"${DEF}"
+  curl -L -o /tmp/quicknav-installer.deb "https://github.com/MrDogeBro/quicknav/releases/download/${tag}/${platformList[$platformChoice]}"
+  echo -e "➤ │ ${BLUE}${BOLD}Info${DEF}: Deb file downloaded"
+
+  echo -e "➤ │"
+
+  echo -e "➤ │ ${BLUE}${BOLD}Info${DEF}: Installing deb file..."
+  echo -e "➤ │ ${DIM}$ apt install -y /tmp/quicknav-installer.deb${DEF}"
+  apt install -y /tmp/quicknav-installer.deb
+  echo -e "➤ │ ${BLUE}${BOLD}Info${DEF}: Deb file installed"
+
+  echo -e "➤ │"
+
+  echo -e "➤ │ ${BLUE}${BOLD}Info${DEF}: Cleaning up install..."
+  echo -e "➤ │ ${DIM}$ rm -f /tmp/quicknav-installer.deb${DEF}"
+  rm -f /tmp/quicknav-installer.deb
+  echo -e "➤ │ ${BLUE}${BOLD}Info${DEF}: Cleaned install"
+
+  echo -e "➤ └ Installation complete"
+
+  echo -e "\nThank you for installing quicknav! To get started head follow the instructions on the github page to add quicknav to your shell."
+  echo -e "\n${GREEN}Adding Quicknav to Your Shell${DEF}: https://github.com/MrDogeBro/quicknav#adding-quicknav-to-your-shell"
+else
+  echo -e "${RED}It appears you are not on a debian based system. This script only works on debian based systems. Please check out the section on installing quicknav to get instructions for your OS. Thanks! \n\n${GREEN}Installing Quicknav${DEF}: https://github.com/MrDogeBro/quicknav#installing-quicknav"
+fi
