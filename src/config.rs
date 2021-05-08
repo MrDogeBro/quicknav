b5f1b96e3acd0db9f0187623b2b76ec47303912a
MrDogeBro <MrDogeBro@users.noreply.github.com>
2021-05-08 19:50:58 -0400

    Use anyhow for error handling (part of #2)

--- src/config.rs
+++ src/config.rs
@@ -1,9 +1,8 @@
-use colored::*;
+use anyhow::Result;
 use std::env::var;
 use std::fs;
 use std::fs::File;
 use std::path::Path;
-use std::process::exit;
 
 #[derive(Serialize, Deserialize)]
 pub struct Shortcut {
@@ -24,14 +23,14 @@ pub struct Config {
     pub options: Options,
 }
 
-fn generate_config() {
+fn generate_config() -> Result<i32> {
     let config_folder = var("XDG_CONFIG_HOME")
         .or_else(|_| var("HOME").map(|home| format!("{}/.config", home)))
         .unwrap();
     let config_path = format!("{}/quicknav", config_folder);
 
     if !Path::new(&config_path).exists() {
-        fs::create_dir(&config_path).expect("Error: Unable to generate config directory.");
+        fs::create_dir(&config_path)?;
     }
 
     fs::write(
@@ -42,43 +41,34 @@ fn generate_config() {
         "create_missing_directories": false
     }
 }"#,
-    )
-    .expect("Error: Unable to generate config.");
+    )?;
+
+    Ok(0)
 }
 
-pub fn load_config() -> Config {
+pub fn load_config() -> Result<Config> {
     let config_folder = var("XDG_CONFIG_HOME")
         .or_else(|_| var("HOME").map(|home| format!("{}/.config", home)))
         .unwrap();
     let config_path = format!("{}/quicknav/quicknav.json", config_folder);
 
     if !Path::new(&config_path).exists() {
-        generate_config();
+        generate_config()?;
     }
 
-    let data = File::open(config_path).expect("Error: Unable to open config file.");
-    let raw_config = serde_json::from_reader(data);
+    let data = File::open(config_path)?;
+    let config: Config = serde_json::from_reader(data)?;
 
-    if let Err(e) = raw_config {
-        println!(
-            "{} {}.",
-            "Error: Unable to parse config file. Please make sure that all feilds are present. Traceback:"
-                .red(),
-                e.to_string().red()
-        );
-        exit(1);
-    }
-
-    let config: Config = raw_config.expect("Error: Unable to parse config file.");
-    config
+    Ok(config)
 }
 
-pub fn update_config(config: Config) {
+pub fn update_config(config: Config) -> Result<i32> {
     let config_folder = var("XDG_CONFIG_HOME")
         .or_else(|_| var("HOME").map(|home| format!("{}/.config", home)))
         .unwrap();
     let config_path = format!("{}/quicknav/quicknav.json", config_folder);
 
-    fs::write(config_path, serde_json::to_string_pretty(&config).unwrap())
-        .expect("Error: Failed to write config to file.");
+    fs::write(config_path, serde_json::to_string_pretty(&config).unwrap())?;
+
+    Ok(0)
 }
