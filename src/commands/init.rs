8f2529066ac9ab6591a0a0b0f02f6bf5fe701f45
MrDogeBro <MrDogeBro@users.noreply.github.com>
2021-05-09 17:56:56 -0400

    Move completions to init file (#11) and use command structure from new file

--- src/commands/init.rs
+++ src/commands/init.rs
@@ -1,4 +1,11 @@
 use anyhow::Result;
+use gag::BufferRedirect;
+use quicknav::Quicknav;
+use std::io::Read;
+use structopt::clap::Shell;
+use structopt::StructOpt;
+
+use crate::quicknav;
 
 fn get_profile(profile: &str, command: &str) -> Result<String> {
     if profile == "default" {
@@ -24,7 +31,50 @@ fn get_profile(profile: &str, command: &str) -> Result<String> {
     Ok("".to_string())
 }
 
+fn gen_completions(shell: String) -> Result<i32> {
+    let mut shell_profile = Shell::Bash;
+
+    if shell == "bash" {
+        shell_profile = Shell::Bash;
+    } else if shell == "zsh" {
+        shell_profile = Shell::Bash;
+
+        let mut stdout_buf = BufferRedirect::stdout().unwrap();
+        Quicknav::clap().gen_completions_to("quicknav", shell_profile, &mut std::io::stdout());
+
+        let mut completions = String::new();
+        stdout_buf.read_to_string(&mut completions).unwrap();
+        drop(stdout_buf);
+
+        println!(
+            "autoload bashcompinit\nbashcompinit\n\n{}",
+            completions.replace(
+                "complete -F _quicknav -o bashdefault -o default quicknav",
+                "$(autoload | grep -q bashcompinit) && \
+                 complete -F _quicknav -o bashdefault -o default quicknav"
+            )
+        );
+
+        return Ok(0);
+    } else if shell == "fish" {
+        shell_profile = Shell::Fish;
+    }
+
+    Quicknav::clap().gen_completions_to("quicknav", shell_profile, &mut std::io::stdout());
+
+    Ok(0)
+}
+
 pub fn init(shell: String, command: Option<String>) -> Result<i32> {
+    let supported_shells = vec!["bash", "zsh", "fish"];
+
+    if supported_shells.iter().any(|&s| s == shell) {
+        gen_completions(shell.to_owned())?;
+    } else {
+        println!("echo -e \"\\033[0;31mError: Failed to load shell profile. Invalid or unsupported shell provided.\"");
+        return Ok(1);
+    }
+
     let mut profile: &str = "default";
 
     if shell == "bash" || shell == "zsh" {
