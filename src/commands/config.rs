01a8e0f1ab6b40663efd8cd8af2fb5a009db274a
MrDogeBro <MrDogeBro@users.noreply.github.com>
2021-05-09 19:05:10 -0400

    Get value from config and output to console

--- src/commands/config.rs
+++ src/commands/config.rs
@@ -1,5 +1,21 @@
 use anyhow::Result;
+use colored::*;
+
+use crate::config;
 
 pub fn config(option: String) -> Result<i32> {
+    let config: config::Config = config::Config::load()?;
+    let value;
+
+    match option.as_str() {
+        "create_missing_directories" => value = config.options.create_missing_directories,
+        _ => {
+            println!("{}", "Error: Option not found or is not valid. Use quicknav config to view available options.".red());
+            return Ok(1);
+        }
+    }
+
+    println!("Current value: {}", value);
+
     Ok(0)
 }
