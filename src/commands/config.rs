8ea5dc85742e72d64a92035edc17cd1fabecdb7d
MrDogeBro <MrDogeBro@users.noreply.github.com>
2021-05-09 20:37:15 -0400

    Add logic to update config if new value provided

--- src/commands/config.rs
+++ src/commands/config.rs
@@ -3,6 +3,7 @@ use colored::*;
 use prettytable::{format, Table};
 
 use crate::config;
+use crate::utils::string;
 
 pub fn config(option: Option<String>, new_value: Option<String>) -> Result<i32> {
     let config: config::Config = config::Config::load()?;
@@ -12,6 +13,31 @@ pub fn config(option: Option<String>, new_value: Option<String>) -> Result<i32> 
     option_list.set_titles(row!["Option", "Current Value"]);
 
     if let Some(option) = option {
+        if let Some(new_value) = new_value {
+            let value: &str;
+
+            match option.as_str() {
+                "create_missing_directories" => {
+                    config.options.create_missing_directories = string::to_bool(new_value)?;
+                    value = "create_missing_directories";
+                }
+                _ => {
+                    println!("{}", "Error: Option not found or is not valid. Use quicknav config to view available options.".red());
+                    return Ok(1);
+                }
+            }
+
+            println!(
+                "{} {} -> {}",
+                "Config value updated:".green(),
+                value,
+                new_value
+            );
+
+            config.update();
+            return Ok(0);
+        }
+
         match option.as_str() {
             "create_missing_directories" => {
                 option_list.add_row(row![
