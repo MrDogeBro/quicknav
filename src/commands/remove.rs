5105b657622d0ac6d2002d63c5f18d111661d99a
MrDogeBro <MrDogeBro@users.noreply.github.com>
2021-07-11 12:14:35 -0400

    Add function to handle remove call command

--- src/commands/remove.rs
+++ src/commands/remove.rs
@@ -28,3 +28,36 @@ pub fn remove(shortcut: String) -> Result<i32> {
 
     Ok(0)
 }
+
+pub fn remove_call(call: String) -> Result<i32> {
+    let mut config: config::Config = config::Config::load()?;
+    let mut found_shortcut = false;
+    let mut shortcut_index: usize = 0;
+    let mut call_index: usize = 0;
+
+    for (i, shortcut_conf) in &mut config.shortcuts.iter().enumerate() {
+        if shortcut_conf.calls.iter().any(|c| c == &call) {
+            found_shortcut = true;
+            shortcut_index = i;
+
+            for (i, call_conf) in shortcut_conf.calls.iter().enumerate() {
+                if call_conf == &call {
+                    call_index = i
+                }
+            }
+        }
+    }
+
+    if !found_shortcut {
+        return Err(anyhow!(format!(
+            "Shortcut with call {} was not found.",
+            call,
+        )));
+    }
+
+    config.shortcuts[shortcut_index].calls.remove(call_index);
+    config.update()?;
+    println!("{} {}", "Call removed:".green(), &call);
+
+    Ok(0)
+}
