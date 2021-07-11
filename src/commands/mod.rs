88abb70193080d1e6937880d83572518d29c2cfb
MrDogeBro <MrDogeBro@users.noreply.github.com>
2021-07-11 12:14:57 -0400

    Include add and remove call commands in commands

--- src/commands/mod.rs
+++ src/commands/mod.rs
@@ -6,8 +6,10 @@ mod list;
 mod remove;
 
 pub use add::add;
+pub use add::add_call;
 pub use config::config;
 pub use get::get;
 pub use init::init;
 pub use list::list;
 pub use remove::remove;
+pub use remove::remove_call;
