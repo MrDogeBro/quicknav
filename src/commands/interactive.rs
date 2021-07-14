cadd523b28917cc8ac81f16dd17917137554a79f
Jonx <jon@jonxtech.com>
2021-07-13 21:38:10 -0700

    Add helpful implementations to Context
    Add Line enum
    
    Fix bugs in backspace key parsing
    
    Newlines working nicely
    
    Add docstrings and comments

--- src/commands/interactive.rs
+++ src/commands/interactive.rs
@@ -1,9 +1,8 @@
 use anyhow::{anyhow, Result};
 
+use std::fmt;
 use std::fs::File;
-use std::io::{self, BufRead, Read, Stdin, Stdout, Write};
-use std::collections::HashMap;
-use colored::*;
+use std::io::{self, Write};
 
 use termion;
 use termion::{get_tty, color, clear, cursor};
@@ -12,198 +11,342 @@ use termion::event::Key;
 use termion::raw::{IntoRawMode, RawTerminal};
 use termion::screen::*;
 
+
+/// Enum representing the types we will write to the TTY
+enum Line {
+
+    /// Represents a char
+    Char(char),
+
+    /// Represents a String
+    Str(String),
+}
+
+impl Line {
+
+    /// Returns the length of the line
+    fn len(&self) -> u16 {
+
+        match self {
+
+            Line::Str(s) => s.len() as u16,
+            _ => 1,
+        }
+    }
+}
+
+impl fmt::Display for Line {
+
+    /// Implements Display Trait for the Line enum
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> Result<(), fmt::Error> {
+
+        match self {
+
+            Line::Str(s) => write!(f, "{}", s)?,
+            Line::Char(c) => write!(f, "{}", c)?,
+        }
+
+        Ok(())
+    }
+}
+
+/// Structure representing the context of the shell
 struct Context {
+
+    /// The TTY to read from and write to
     tty: File,
+
+    /// The current line number
     line: u16,
+
+    /// The current column number
     column: u16,
+
+    /// Buffer containing String of chars the user has entered on the current line
     content: String,
+
+    /// A check for proceeding to the next phase of the interactive shell
     check: bool,
+
+    /// The side of the terminal window
     size: (u16, u16),
-    shell: AlternateScreen<RawTerminal<Stdout>>,
-    stdin: Stdin,
+
+    /// The alternate screen we are reading from and writing to
+    shell: AlternateScreen<RawTerminal<io::Stdout>>,
 }
 
 impl Context {
+
+    /// Constructs a new Context
     fn new(tty: File) -> Self {
+
         Context {
+
             tty,
             line: 3,
             column: 1,
             content: String::new(),
             check: false,
             size: termion::terminal_size().unwrap(),
-            stdin: io::stdin(),
             shell: AlternateScreen::from(
                 io::stdout().into_raw_mode().unwrap()
-            )
+            ),
         }
     }
 
-    fn write<T>(&self, string: &str, args: &[T]) -> Result<i32> {
-        // TODO
-        Ok(0)
+    /// Initializes the shell
+    fn init_shell(&mut self) -> Result<()>{
+
+        write!(
+
+            self.shell,
+            "{}{}{}Welcome to the quicknav interactive shell.{}{}{}",
+            clear::All,
+            cursor::Goto(1, 1),
+            color::Fg(color::Green),
+            color::Fg(color::Reset),
+            cursor::Goto(1, 2),
+            "-".repeat(42),
+        )?;
+
+        write!(
+
+            self.shell,
+            "{}{} Ctrl+c{} {}to exit{}{}{}",
+            cursor::Goto(self.size.0 + 2, self.size.1 - 2),
+            termion::style::Bold,
+            termion::style::Reset,
+            color::Fg(color::Red),
+            termion::style::Reset,
+            color::Fg(color::Reset),
+            cursor::Goto(1, 3),
+        )?;
+
+        self.shell.flush()?;
+
+        Ok(())
+    }
+
+    /// Pushes a char onto the buffer storing the current lines content
+    fn push(&mut self, c: char) {
+
+        self.content.push(c);
+    }
+
+    /// Pops the last char from the buffer storing the current lines content
+    fn pop(&mut self) {
+
+        self.content.pop();
+    }
+
+    /// Writes a char to the TTY
+    fn write_char(&mut self, c: char) -> Result<()> {
+
+        self.column += 1;
+        self.push(c);
+
+        write!(self.tty, "{}", c)?;
+
+        Ok(())
+    }
+
+    /// Writes a line to the TTY
+    fn write_line(&mut self, line: Line) -> Result<()> {
+
+        write!(self.tty, "{}", line)?;
+        self.new_line()?;
+        self.shell.flush()?;
+
+        Ok(())
+    }
+
+    /// Write a new line to the TTY
+    fn new_line(&mut self) -> Result<()> {
+
+        // Position the cursor one line down, in the first column
+        self.column = 1;
+        self.line += 1;
+
+        write!(
+            self.tty, "{}",
+            cursor::Goto(self.column, self.line),
+        )?;
+
+        Ok(())
+    }
+
+    /// Empties the buffer containing the current lines content
+    fn purge(&mut self) {
+
+        self.content = String::new();
+    }
+
+    /// Deletes the character following the cursor from the TTY
+    fn del(&mut self) -> Result<()> {
+
+        Ok(())
+    }
+
+    /// Deletes the character preceding the cursor from the TTY
+    fn backspace(&mut self) -> Result<()> {
+
+        match self.column {
+
+            // Prevents going off the left side of the terminal
+            1 => {},
+            _ => {
+
+                self.column -= 1;
+                self.pop();
+
+                write!(self.tty,
+                    "{}{}{}", cursor::Goto(self.column, self.line),
+                    " ", cursor::Goto(self.column, self.line),
+                )?;
+            }
+        }
+
+        Ok(())
     }
 }
 
+/// Public facing entry point to the interactive shell
 pub fn shell(command: Option<String>) -> Result<i32> {
-    interactive_shell(command)
+
+    interactive_shell()
 }
 
-fn init_shell(ctx: &mut Context) {
-    write!(
-        ctx.tty,
-        "{}{}{}Welcome to the quicknav interactive shell.{}",
-        clear::All,
-        cursor::Goto(1, 1),
-        color::Fg(color::Green),
-        color::Fg(color::Reset),
-    ).unwrap();
+fn interactive_shell() -> Result<i32> {
 
-    write!(
-        ctx.tty,
-        "{}{} Ctrl+c{} {}to exit{}{}{}",
-        cursor::Goto(ctx.size.0 + 5, ctx.size.1 - 2),
-        termion::style::Bold,
-        termion::style::Reset,
-        color::Fg(color::Red),
-        termion::style::Reset,
-        color::Fg(color::Reset),
-        cursor::Goto(1, 3),
-    ).unwrap();
-
-    ctx.tty.flush().unwrap();
-}
-
-fn interactive_shell(command: Option<String>) -> Result<i32> {
+    // Initialize a new shell and Context
+    let stdin = io::stdin();
     let mut ctx = Context::new(get_tty()?);
-    init_shell(&mut ctx);
+    ctx.init_shell()?;
 
-    for c in ctx.stdin.keys().map(|c| c.unwrap()) {
-        ctx.column += 1;
+    // Iterate over key presses and act accordingly
+    for c in stdin.keys().map(|c| c.unwrap()) {
 
         match c {
+
             Key::Ctrl('c') => break,
+            Key::Backspace => ctx.backspace()?,
+            Key::Delete => ctx.del()?,
+            Key::Up => {}
+            Key::Down => {}
+            Key::Left => {}
+            Key::Right => {}
             Key::Char('\n') => {
-                ctx.column = 1;
-                ctx.line += 1;
-                write!(ctx.tty, "{}", cursor::Goto(ctx.column, ctx.line))?;
+                ctx.new_line()?;
+                ctx.write_line(Line::Str(format!("Content: {}", ctx.content.clone())))?; // just testing this here
+                ctx.new_line()?;
+                ctx.purge();
             }
-            Key::Backspace => {
-                match ctx.column {
-                    1 | 2 => {},
-                    _ => {
-                        ctx.column -= 2;
-                        write!(ctx.tty, "{}{}", termion::clear::CurrentLine, cursor::Goto(ctx.column, ctx.line))?;
-                        ctx.content.pop();
-                    }
-                }
-            }
-            Key::Char('0') => write!(ctx.tty, "{}", '0')?,
-            Key::Char('1') => write!(ctx.tty, "{}", '1')?,
-            Key::Char('2') => write!(ctx.tty, "{}", '2')?,
-            Key::Char('3') => write!(ctx.tty, "{}", '3')?,
-            Key::Char('4') => write!(ctx.tty, "{}", '4')?,
-            Key::Char('5') => write!(ctx.tty, "{}", '5')?,
-            Key::Char('6') => write!(ctx.tty, "{}", '6')?,
-            Key::Char('7') => write!(ctx.tty, "{}", '7')?,
-            Key::Char('8') => write!(ctx.tty, "{}", '8')?,
-            Key::Char('9') => write!(ctx.tty, "{}", '9')?,
-            Key::Char('a') => write!(ctx.tty, "{}", 'a')?,
-            Key::Char('b') => write!(ctx.tty, "{}", 'b')?,
-            Key::Char('c') => write!(ctx.tty, "{}", 'c')?,
-            Key::Char('d') => write!(ctx.tty, "{}", 'd')?,
-            Key::Char('e') => write!(ctx.tty, "{}", 'e')?,
-            Key::Char('f') => write!(ctx.tty, "{}", 'f')?,
-            Key::Char('g') => write!(ctx.tty, "{}", 'g')?,
-            Key::Char('h') => write!(ctx.tty, "{}", 'h')?,
-            Key::Char('i') => write!(ctx.tty, "{}", 'i')?,
-            Key::Char('j') => write!(ctx.tty, "{}", 'j')?,
-            Key::Char('k') => write!(ctx.tty, "{}", 'k')?,
-            Key::Char('l') => write!(ctx.tty, "{}", 'l')?,
-            Key::Char('m') => write!(ctx.tty, "{}", 'm')?,
-            Key::Char('n') => write!(ctx.tty, "{}", 'n')?,
-            Key::Char('o') => write!(ctx.tty, "{}", 'o')?,
-            Key::Char('p') => write!(ctx.tty, "{}", 'p')?,
-            Key::Char('q') => write!(ctx.tty, "{}", 'q')?,
-            Key::Char('r') => write!(ctx.tty, "{}", 'r')?,
-            Key::Char('s') => write!(ctx.tty, "{}", 's')?,
-            Key::Char('t') => write!(ctx.tty, "{}", 't')?,
-            Key::Char('u') => write!(ctx.tty, "{}", 'u')?,
-            Key::Char('v') => write!(ctx.tty, "{}", 'v')?,
-            Key::Char('w') => write!(ctx.tty, "{}", 'w')?,
-            Key::Char('x') => write!(ctx.tty, "{}", 'x')?,
-            Key::Char('y') => write!(ctx.tty, "{}", 'y')?,
-            Key::Char('z') => write!(ctx.tty, "{}", 'z')?,
-            Key::Char('A') => write!(ctx.tty, "{}", 'A')?,
-            Key::Char('B') => write!(ctx.tty, "{}", 'B')?,
-            Key::Char('C') => write!(ctx.tty, "{}", 'C')?,
-            Key::Char('D') => write!(ctx.tty, "{}", 'D')?,
-            Key::Char('E') => write!(ctx.tty, "{}", 'E')?,
-            Key::Char('F') => write!(ctx.tty, "{}", 'F')?,
-            Key::Char('G') => write!(ctx.tty, "{}", 'G')?,
-            Key::Char('H') => write!(ctx.tty, "{}", 'H')?,
-            Key::Char('I') => write!(ctx.tty, "{}", 'I')?,
-            Key::Char('J') => write!(ctx.tty, "{}", 'J')?,
-            Key::Char('K') => write!(ctx.tty, "{}", 'K')?,
-            Key::Char('L') => write!(ctx.tty, "{}", 'L')?,
-            Key::Char('M') => write!(ctx.tty, "{}", 'M')?,
-            Key::Char('N') => write!(ctx.tty, "{}", 'N')?,
-            Key::Char('O') => write!(ctx.tty, "{}", 'O')?,
-            Key::Char('P') => write!(ctx.tty, "{}", 'P')?,
-            Key::Char('Q') => write!(ctx.tty, "{}", 'Q')?,
-            Key::Char('R') => write!(ctx.tty, "{}", 'R')?,
-            Key::Char('S') => write!(ctx.tty, "{}", 'S')?,
-            Key::Char('T') => write!(ctx.tty, "{}", 'T')?,
-            Key::Char('U') => write!(ctx.tty, "{}", 'U')?,
-            Key::Char('V') => write!(ctx.tty, "{}", 'V')?,
-            Key::Char('W') => write!(ctx.tty, "{}", 'W')?,
-            Key::Char('X') => write!(ctx.tty, "{}", 'X')?,
-            Key::Char('Y') => write!(ctx.tty, "{}", 'Y')?,
-            Key::Char('Z') => write!(ctx.tty, "{}", 'Z')?,
-            Key::Char('!') => write!(ctx.tty, "{}", '!')?,
-            Key::Char('"') => write!(ctx.tty, "{}", '"')?,
-            Key::Char('#') => write!(ctx.tty, "{}", '#')?,
-            Key::Char('$') => write!(ctx.tty, "{}", '$')?,
-            Key::Char('%') => write!(ctx.tty, "{}", '%')?,
-            Key::Char('&') => write!(ctx.tty, "{}", '&')?,
-            Key::Char('(') => write!(ctx.tty, "{}", '(')?,
-            Key::Char(')') => write!(ctx.tty, "{}", ')')?,
-            Key::Char('*') => write!(ctx.tty, "{}", '*')?,
-            Key::Char('+') => write!(ctx.tty, "{}", '+')?,
-            Key::Char(',') => write!(ctx.tty, "{}", ',')?,
-            Key::Char('-') => write!(ctx.tty, "{}", '-')?,
-            Key::Char('.') => write!(ctx.tty, "{}", '.')?,
-            Key::Char('/') => write!(ctx.tty, "{}", '/')?,
-            Key::Char(':') => write!(ctx.tty, "{}", ':')?,
-            Key::Char(';') => write!(ctx.tty, "{}", ';')?,
-            Key::Char('<') => write!(ctx.tty, "{}", '<')?,
-            Key::Char('=') => write!(ctx.tty, "{}", '=')?,
-            Key::Char('>') => write!(ctx.tty, "{}", '>')?,
-            Key::Char('?') => write!(ctx.tty, "{}", '?')?,
-            Key::Char('@') => write!(ctx.tty, "{}", '@')?,
-            Key::Char('[') => write!(ctx.tty, "{}", '[')?,
-            Key::Char(']') => write!(ctx.tty, "{}", ']')?,
-            Key::Char('^') => write!(ctx.tty, "{}", '^')?,
-            Key::Char('_') => write!(ctx.tty, "{}", '_')?,
-            Key::Char('`') => write!(ctx.tty, "{}", '`')?,
-            Key::Char('{') => write!(ctx.tty, "{}", '{')?,
-            Key::Char('|') => write!(ctx.tty, "{}", '|')?,
-            Key::Char('}') => write!(ctx.tty, "{}", '}')?,
-            Key::Char('~') => write!(ctx.tty, "{}", '~')?,
-            Key::Char(' ') => write!(ctx.tty, "{}", ' ')?,
-            Key::Char('\'') => write!(ctx.tty, "{}", '\'')?,
-            Key::Char('\\') => write!(ctx.tty, "{}", '\\')?,
-            Key::Char('\t') => write!(ctx.tty, "{}", '\t')?,
-            Key::Char('\r') => write!(ctx.tty, "{}", '\r')?,
+            Key::Char('0') => ctx.write_char('0')?,
+            Key::Char('1') => ctx.write_char('1')?,
+            Key::Char('2') => ctx.write_char('2')?,
+            Key::Char('3') => ctx.write_char('3')?,
+            Key::Char('4') => ctx.write_char('4')?,
+            Key::Char('5') => ctx.write_char('5')?,
+            Key::Char('6') => ctx.write_char('6')?,
+            Key::Char('7') => ctx.write_char('7')?,
+            Key::Char('8') => ctx.write_char('8')?,
+            Key::Char('9') => ctx.write_char('9')?,
+            Key::Char('a') => ctx.write_char('a')?,
+            Key::Char('b') => ctx.write_char('b')?,
+            Key::Char('c') => ctx.write_char('c')?,
+            Key::Char('d') => ctx.write_char('d')?,
+            Key::Char('e') => ctx.write_char('e')?,
+            Key::Char('f') => ctx.write_char('f')?,
+            Key::Char('g') => ctx.write_char('g')?,
+            Key::Char('h') => ctx.write_char('h')?,
+            Key::Char('i') => ctx.write_char('i')?,
+            Key::Char('j') => ctx.write_char('j')?,
+            Key::Char('k') => ctx.write_char('k')?,
+            Key::Char('l') => ctx.write_char('l')?,
+            Key::Char('m') => ctx.write_char('m')?,
+            Key::Char('n') => ctx.write_char('n')?,
+            Key::Char('o') => ctx.write_char('o')?,
+            Key::Char('p') => ctx.write_char('p')?,
+            Key::Char('q') => ctx.write_char('q')?,
+            Key::Char('r') => ctx.write_char('r')?,
+            Key::Char('s') => ctx.write_char('s')?,
+            Key::Char('t') => ctx.write_char('t')?,
+            Key::Char('u') => ctx.write_char('u')?,
+            Key::Char('v') => ctx.write_char('v')?,
+            Key::Char('w') => ctx.write_char('w')?,
+            Key::Char('x') => ctx.write_char('x')?,
+            Key::Char('y') => ctx.write_char('y')?,
+            Key::Char('z') => ctx.write_char('z')?,
+            Key::Char('A') => ctx.write_char('A')?,
+            Key::Char('B') => ctx.write_char('B')?,
+            Key::Char('C') => ctx.write_char('C')?,
+            Key::Char('D') => ctx.write_char('D')?,
+            Key::Char('E') => ctx.write_char('E')?,
+            Key::Char('F') => ctx.write_char('F')?,
+            Key::Char('G') => ctx.write_char('G')?,
+            Key::Char('H') => ctx.write_char('H')?,
+            Key::Char('I') => ctx.write_char('I')?,
+            Key::Char('J') => ctx.write_char('J')?,
+            Key::Char('K') => ctx.write_char('K')?,
+            Key::Char('L') => ctx.write_char('L')?,
+            Key::Char('M') => ctx.write_char('M')?,
+            Key::Char('N') => ctx.write_char('N')?,
+            Key::Char('O') => ctx.write_char('O')?,
+            Key::Char('P') => ctx.write_char('P')?,
+            Key::Char('Q') => ctx.write_char('Q')?,
+            Key::Char('R') => ctx.write_char('R')?,
+            Key::Char('S') => ctx.write_char('S')?,
+            Key::Char('T') => ctx.write_char('T')?,
+            Key::Char('U') => ctx.write_char('U')?,
+            Key::Char('V') => ctx.write_char('V')?,
+            Key::Char('W') => ctx.write_char('W')?,
+            Key::Char('X') => ctx.write_char('X')?,
+            Key::Char('Y') => ctx.write_char('Y')?,
+            Key::Char('Z') => ctx.write_char('Z')?,
+            Key::Char('!') => ctx.write_char('!')?,
+            Key::Char('"') => ctx.write_char('"')?,
+            Key::Char('#') => ctx.write_char('#')?,
+            Key::Char('$') => ctx.write_char('$')?,
+            Key::Char('%') => ctx.write_char('%')?,
+            Key::Char('&') => ctx.write_char('&')?,
+            Key::Char('(') => ctx.write_char('(')?,
+            Key::Char(')') => ctx.write_char(')')?,
+            Key::Char('*') => ctx.write_char('*')?,
+            Key::Char('+') => ctx.write_char('+')?,
+            Key::Char(',') => ctx.write_char(',')?,
+            Key::Char('-') => ctx.write_char('-')?,
+            Key::Char('.') => ctx.write_char('.')?,
+            Key::Char('/') => ctx.write_char('/')?,
+            Key::Char(':') => ctx.write_char(':')?,
+            Key::Char(';') => ctx.write_char(';')?,
+            Key::Char('<') => ctx.write_char('<')?,
+            Key::Char('=') => ctx.write_char('=')?,
+            Key::Char('>') => ctx.write_char('>')?,
+            Key::Char('?') => ctx.write_char('?')?,
+            Key::Char('@') => ctx.write_char('@')?,
+            Key::Char('[') => ctx.write_char('[')?,
+            Key::Char(']') => ctx.write_char(']')?,
+            Key::Char('^') => ctx.write_char('^')?,
+            Key::Char('_') => ctx.write_char('_')?,
+            Key::Char('`') => ctx.write_char('`')?,
+            Key::Char('{') => ctx.write_char('{')?,
+            Key::Char('|') => ctx.write_char('|')?,
+            Key::Char('}') => ctx.write_char('}')?,
+            Key::Char('~') => ctx.write_char('~')?,
+            Key::Char(' ') => ctx.write_char(' ')?,
+            Key::Char('\'') => ctx.write_char('\'')?,
+            Key::Char('\\') => ctx.write_char('\\')?,
+            Key::Char('\t') => ctx.write_char('\t')?,
+            Key::Char('\r') => ctx.write_char('\r')?,
             _ => {write!(ctx.tty, "bad {:?}", c)?},
          }
 
-        ctx.tty.flush()?;
+        ctx.shell.flush()?;
     }
 
-
+    // Exit the interactive shell
     write!(ctx.tty, "{}", ToMainScreen)?;
-    ctx.tty.flush()?;
+    ctx.shell.flush()?;
 
     Ok(0)
 }
