7d5f8a48e9f1eeabac0ff5f7a1f592faa038db30
MrDogeBro <35872177+MrDogeBro@users.noreply.github.com>
2021-07-17 02:37:40 +0000

    Put shortcut name before calls in list command
    Co-authored-by: Jonxslays <51417989+Jonxslays@users.noreply.github.com>

--- src/commands/list.rs
+++ src/commands/list.rs
@@ -9,12 +9,12 @@ pub fn list(shortcut: Option<String>) -> Result<i32> {
 
         let mut shortcut_list = Table::new();
         shortcut_list.set_format(*format::consts::FORMAT_NO_BORDER_LINE_SEPARATOR);
-        shortcut_list.set_titles(row!["Shortcuts", "Shortcut Name", "Shortcut Location"]);
+        shortcut_list.set_titles(row!["Name", "Shortcuts", "Shortcut Location"]);
 
         for shortcut_conf in config.shortcuts {
             if shortcut_conf.name.to_lowercase() == shortcut.to_lowercase() {
                 let calls: String = shortcut_conf.calls.join(", ");
-                shortcut_list.add_row(row![calls, shortcut_conf.name, shortcut_conf.location]);
+                shortcut_list.add_row(row![shortcut_conf.name, calls, shortcut_conf.location]);
                 shortcut_list.printstd();
                 return Ok(0);
             }
